222070d9135d76085ec137cc3c6d43d4fe45b68f
BALAKRISHNAN <abinaya.a.balakrishnan@capgemini.com>
2023-06-22 15:53:57 +0530

    new changes

--- DemoProject/src/demopackage/DemoClass.java
+++ DemoProject/src/demopackage/DemoClass.java
@@ -1,10 +1,21 @@
 package demopackage;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class DemoClass {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello from GIT 1!!");
+		String path = System.getProperty("user.dir");
+		System.setProperty("webdriver.chrome.driver", path +"./drivers/chromedriver.exe");
+		System.out.println("");
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://amazon.in");
+		boolean logo = driver.findElement(By.id("nav-logo-sprites")).isDisplayed();	
+		if(logo ==true)
+		{
+			System.out.println("Logo displayed");
+		}
 	}
-
 }
